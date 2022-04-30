eb66372d6c26c363c47cde569ddce153614bc8a8
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-04-30 13:01:01 -0300

    Feat: Getting main section finished.

--- facebook-clone/src/components/Form/Form.js
+++ facebook-clone/src/components/Form/Form.js
@@ -1,7 +1,9 @@
 export default function Form(){
   return(
-    <form>
-
+    <form actio="" className="flex flex-col">
+      <input type="text" placeholder="Email ou telefone" class="flex-1 mb-2 md:mb-3 p-3 rounded md:rounded-md border border-gray-400 bg-gray-200 md:bg-white text-sm md:text-base placeholder-gray-600 md:placeholder-gray-500" />
+      <input type="password" placeholder="Senha" class="flex-1 mb-3 p-3 rounded md:rounded-md border border-gray-400 bg-gray-200 md:bg-white text-sm md:text-base placeholder-gray-600 md:placeholder-gray-500" />
+      <input type="submit" value="Entrar" class="flex-1 mb-3 py-2 px-3 rounded-md bg-main-blue hover:bg-main-blue-hover font-semibold text-white text-xl" />
     </form>
   );
 }
--- facebook-clone/src/components/Main/Main.js
+++ facebook-clone/src/components/Main/Main.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import Form from "../Form/Form";
 import Facebook from '../../assets/img/logo_facebook.svg'
 
@@ -11,20 +12,20 @@ export default function Main(){
             <p className="hidden md:flex text-subtitle leading-8 w-500px pr-6">O Facebook ajuda você a se conectar e compartilhar com as pessoas que fazem parte da sua vida.</p>
           </div>
           <div className="md:w-396px md:place-self-end">
-            <div >
+            <div className="p-4 md:border md:shadow-xl rounder-md bg-white">
               <Form />
-              <div  >
-                <div >
-                  <a href=""  >Esqueceu a senha?</a>
+              <div className="flex flex-col divide-y divide-gray-400 text-center mt-2 md:mt-0" >
+                <div className="pb-4 order-2 md:order-1">
+                  <a href="" className="text-blue-600 hover:underline">Esqueceu a senha?</a>
                 </div>
-                <div >
-                  <a href="#"  >Criar nova conta</a>
+                <div className="pb-6 pt-8 order-1 md:order-2">
+                  <a href="#" className="py-3 px-4 bg-main-green hover:bg-main-green-hover text-white text-md md:text-lg font-bold rounded">Criar nova conta</a>
                 </div>
               </div>
             </div>
-            <div  >
-              <p><a href="#" >Criar uma Página</a> para uma celebridade, banda ou empresa.</p>
-            </div>
+            <div class="hidden md:block text-sm mt-5 text-center">
+            <p><a href="#" class="font-bold">Criar uma Página</a> para uma celebridade, banda ou empresa.</p>
+          </div>
           </div>
         </div>
     </div>
