434cf220e0620545f12268f58fc4322afda1dde8
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-04-30 13:37:41 -0300

    Feat: Finishing the footer and the clone of Facebook.

--- facebook-clone/src/pages/HomePage.js
+++ facebook-clone/src/pages/HomePage.js
@@ -1,9 +1,11 @@
+import Footer from "../components/Footer/Footer";
 import Main from "../components/Main/Main";
 
 export default function HomePage(){
   return(
     <>
       <Main /> 
+      <Footer />
     </>
   );
 }
